5e21ed071a94f69c6d3a89dc7ec3729ca062ee95
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-03 17:32:18 +0800

    refactor(client-shared): simplify

--- packages/client-shared/src/utils/jsonrpc.ts
+++ packages/client-shared/src/utils/jsonrpc.ts
@@ -1,7 +1,7 @@
 import type { JSONRPCRequest } from '../types/jsonrpc'
 
 export const jsonrpcRequest = <T extends Record<string, unknown>>(method: string, params?: T, withoutId?: true): JSONRPCRequest<T> => ({
-  id: !withoutId ? crypto.randomUUID() : undefined,
+  id: withoutId ? undefined : crypto.randomUUID(),
   jsonrpc: '2.0',
   method,
   params,
