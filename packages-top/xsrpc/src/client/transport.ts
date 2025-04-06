6a4860de8927e6e633557a026ff190859087bb9d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-06 17:23:50 +0800

    chore: remove xsrpc

--- packages-top/xsrpc/src/client/transport.ts
+++
@@ -1,9 +0,0 @@
-import type { RPCNotification, RPCRequest, RPCResponse } from '../types/msg'
-
-export abstract class RPCTransport {
-  public abstract notification(notification: RPCNotification): Promise<void> | void
-
-  public abstract request<T extends Record<string, unknown> = Record<string, unknown>>(request: RPCRequest): Promise<RPCResponse<T>> | RPCResponse<T>
-
-  public abstract requestBatch<T extends Record<string, unknown> = Record<string, unknown>>(requests: RPCRequest[]): Promise<RPCResponse<T>[]> | RPCResponse<T>[]
-}
