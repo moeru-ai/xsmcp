f00bc2448a6312af2130a4b8c39b0ac20fda6e38
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-06 23:09:47 +0800

    chore(everything): update

--- examples/everything/README.md
+++ examples/everything/README.md
@@ -1,3 +1,10 @@
 # Everything MCP Server
 
 Similar to [modelcontextprotocol/servers/src/everything](https://github.com/modelcontextprotocol/servers/tree/main/src/everything), but reimplemented based on xsMCP.
+
+## Development
+
+```bash
+# Server is listening on http://localhost:3001
+pnpm -rF @xsmcp/everything start
+```
--- examples/everything/src/index.ts
+++ examples/everything/src/index.ts
@@ -8,5 +8,7 @@ const serverAdapter = createServerAdapter(server.fetch)
 
 // eslint-disable-next-line ts/no-misused-promises
 const httpServer = createServer(serverAdapter)
+// eslint-disable-next-line @masknet/no-top-level, no-console
+console.log('Server is listening on http://localhost:3001')
 // eslint-disable-next-line @masknet/no-top-level
 httpServer.listen(3001)
