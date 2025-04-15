e548f253f3253ddd9143b5d683730cd7108c7e3f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-15 21:45:28 +0800

    chore(xsmcp): add exports

--- packages-top/xsmcp/src/index.ts
+++ packages-top/xsmcp/src/index.ts
@@ -1,1 +1,5 @@
-export default {}
+export * from '@xsmcp/client-http'
+export * from '@xsmcp/client-shared'
+export * from '@xsmcp/server-http'
+export * from '@xsmcp/server-shared'
+export * from '@xsmcp/shared'
