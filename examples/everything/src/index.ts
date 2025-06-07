eae3ff1865b9c11adc2fca21103b9a53c0c31122
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-07 16:55:45 +0800

    fix: lint errors

--- examples/everything/src/index.ts
+++ examples/everything/src/index.ts
@@ -1,10 +1,10 @@
-import { fetch } from '@xsmcp/server-http'
+import { createFetch } from '@xsmcp/server-http'
 import { serve } from 'srvx'
 
 import { server } from './server'
 
 // eslint-disable-next-line @masknet/no-top-level
 serve({
-  fetch: fetch(server),
+  fetch: createFetch(server),
   port: 3001,
 })
