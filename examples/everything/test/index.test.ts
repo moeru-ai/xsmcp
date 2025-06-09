48b51f63b34e739de22dcdd282564a4b6bf319d1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-09 16:34:19 +0800

    chore: fix test

--- examples/everything/test/index.test.ts
+++ examples/everything/test/index.test.ts
@@ -40,7 +40,7 @@ describe('@xsmcp/everything', async () => {
     const complexResult = await client.getPrompt('complex_prompt', { style: 'dark', temperature: '42' })
     expect(complexResult).toMatchSnapshot()
 
-    const resourceResult = await client.getPrompt('resource_prompt', { resourceId: '2s' })
+    const resourceResult = await client.getPrompt('resource_prompt', { resourceId: '2' })
     expect(resourceResult).toMatchSnapshot()
   })
 
